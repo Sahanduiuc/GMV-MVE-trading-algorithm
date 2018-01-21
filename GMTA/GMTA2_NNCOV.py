849eda32df56b2ae403b3414f05d86f2df6c9e87
zhhrozhh <zhangh40@msu.edu>
2018-01-20 20:58:46 -0600

    nncov

--- GMTA/GMTA2_NNCOV.py
+++ GMTA/GMTA2_NNCOV.py
@@ -9,14 +9,21 @@ class GMTA2_NNCOV(GMTA):
         no_short = True,
         optimizer_tol = 1e-15,
         optimizer_maxiter = 600,
-        quandl_apikey = None
+        quandl_apikey = None,
+        period2 = 30,
+        w2 = 0.02,
+        w3 = 0.02
         ):
         params = locals()
+        self.period2 = params.pop('period2')
+        self.w2 = params.pop('w2')
+        self.w3 = params.pop('w3')
         GMTA.__init__(**params)
 
-    def update(self,data = None):
-        if data is None:
-            data = self.data
+    def one_trade(self,datax = None):
+        if datax is None:
+            datax = self.data
+        data = datax[:self.period]
         data = data[self.scodes]
         ss = []
         d0 = data
@@ -29,7 +36,27 @@ class GMTA2_NNCOV(GMTA):
             rs.append(d0)
             ss.append(list(d0.columns))
             d0 = d1
-        return ss,rs
+        aas = pd.DataFrame()
+        ws = {}
+        for s,dt in zip(ss,rs):
+            g = GMTA(
+                scodes = s,
+                Rf = self.Rf,
+                period = self.period2,
+                no_short = self.no_short,
+                optimizer_tol = self.tol,
+                optimizer_maxiter = self.maxiter,
+                quandl_apikey = self.apikey
+                )
+            a,b,_,_ = g.trading_simulator(datax[s],self.w2)
 
+            aas[','.join(s)] = a
+            ws[','.join(s)] = pd.Series(b[-1],index = s)
+        gt = GMTA(scodes = list(aas.columns),period = self.period2)
+        W = pd.Series(gt.one_trade(aas,self.w3),index = aas.columns)
+        res = pd.Series(np.zeros(len(self.scodes)),index = self.scodes)
+        for s in W.index:
+            res.loc[s.split(',')] = W.loc[s]*ws[s]
+        return res
 
 
